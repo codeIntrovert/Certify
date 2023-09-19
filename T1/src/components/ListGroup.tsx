95cbf3fde559683c59b36e73d5285c40fec6a852
codeIntrovert <9131411681hasan@gmail.com>
2023-09-19 03:04:35 -0700

    renamed the project

--- T1/src/components/ListGroup.tsx
+++ T1/src/components/ListGroup.tsx
@@ -2,10 +2,12 @@ import {} from "react";
 
 function ListGroup() {
   let items = ["New York", "Los Angeles", "Chicago", "Houston", "Philadelphia"];
-
+  const Message =
+    items.length === 0 ? <p>There are no items in the list.</p> : null;
   return (
     <>
       <h1>ListGroup</h1>
+      {Message}
       <ul className="list-group">
         {items.map((item) => (
           <li className="list-group-item" key={item}>
