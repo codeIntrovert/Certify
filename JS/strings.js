87d2ac44a098ac726113a7a4d21e06f40ebcdb28
Hasan <9131411681hasan@gmail.com>
2024-05-12 21:48:10 +0530

    Remove unnecessary code files and styles

--- JS/strings.js
+++
@@ -1,9 +0,0 @@
-// making user name genrator
-let fullName = prompt("Enter your full name: ");
-
-let numberOfChar = fullName.length;
-let cleanName = fullName.toLowerCase().trim().replace(/ /g, '');
-
-let username = `Your username is @${cleanName}${numberOfChar}`;
-
-console.log(username);
